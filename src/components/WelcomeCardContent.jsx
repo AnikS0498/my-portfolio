ed938ff3a81cbfe0295820fb4d18de88a3838a55
AnikS0498 <anikeshsinha041198@gmail.com>
2023-03-15 01:00:26 +0530

    bug fix

--- src/components/WelcomeCardContent.jsx
+++ src/components/WelcomeCardContent.jsx
@@ -14,17 +14,12 @@ function WelcomeCardContent() {
         <div style={{ paddingLeft: "2%" }} className="my-3">
           <div>
             Hi, I am Anikesh, a software engineer by profession. A tech
-            enthusiast, always looking to grow professionally and have fun. I
-            always had an interest in developing UIs so now, I am exploring
-            ReactJs (no hate for Angular, even though I do have limited
-            knowledge😅, but would love to explore it as well).
-            {readMore &&
-              `Isn't that great?. Yes, I have my interest in frontend field but
-            that doesn't mean I have no knowledge in backend. Let's have a 
-            overview of my skillset.`}
-            <Button variant="text" onClick={() => setReadMore(!readMore)}>
+            enthusiast, always looking to grow professionally and have fun. 
+            Being a backend java developer, I have keen interest on frontend as well.
+            So, let's have a small overview of my skillset.
+            {/* <Button variant="text" onClick={() => setReadMore(!readMore)}>
               {!readMore ? `Read More...` : `Read less`}
-            </Button>
+            </Button> */}
           </div>
         </div>
       </Typography>
